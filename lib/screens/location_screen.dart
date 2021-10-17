1d32906cfbd4aa7e911d6646accf4e907a49dede
cantomris <c.tomris@gezinomi.com>
2021-10-17 19:36:45 +0300

    ElevatedButtons instead of deprecated FlatButtons

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -55,7 +55,7 @@ class _LocationScreenState extends State<LocationScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  FlatButton(
+                  ElevatedButton(
                     onPressed: () async {
                       var weatherData = await weather.getLocationWeather();
                       updateUI(weatherData);
@@ -65,7 +65,7 @@ class _LocationScreenState extends State<LocationScreen> {
                       size: 50.0,
                     ),
                   ),
-                  FlatButton(
+                  ElevatedButton(
                     onPressed: () {},
                     child: const Icon(
                       Icons.location_city,
